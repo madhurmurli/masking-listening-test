81859a19668aece62f2e2e33fd830144cd1b6280
evanshenkman <evan.shenkman@gmail.com>
2016-10-26 18:48:19 -0400

    Added classes

---
+++ generateTestConditions.m
@@ -0,0 +1,25 @@
+function testConditions = generateTestConditions()
+    %generateTestConditions - generates all combinations of test conditions
+    %outlined in the TestConfig file
+    %
+    % See also TestConfig.m
+    
+    d = dir([TestConfig.AlarmFolder filesep '*.wav']);
+
+    numConditions = length(TestConfig.MaskTypes) * length(TestConfig.MaskLevels) * length(d);
+
+    testConditions = cell(1, numConditions);
+    index = 1;
+
+    for i = 1:length(TestConfig.MaskTypes)
+        for j = 1:length(TestConfig.MaskLevels)
+            for k = 1:length(d)
+
+                testConditions{index} = TestCondition(TestConfig.MaskTypes(i), TestConfig.MaskLevels(j), d(k).name);
+                index = index + 1;
+
+            end
+        end
+    end
+
+end
