aeddc20e93904fd1fbfaf5761a4159489eede3d7
Madhur <mxm916@miami.edu>
2016-10-26 17:48:53 -0400

    added things to make audio asychronously accurate

--- AudioOutputTest3.m
+++ AudioOutputTest3.m
@@ -1,22 +1,36 @@
 fs=16000;
-hwhite = dsp.ColoredNoise('Color','pink','SamplesPerFrame',8000);
-rng default;
-x = step(hwhite);
-x=x./max(abs(x));
-x=x./1.1;
-p=0;
+%hwhite = dsp.ColoredNoise('Color','pink','SamplesPerFrame',8000);
+%rng default;
+%x = step(hwhite);
+%x=x./max(abs(x));
+%x=x./1.1;
 %%
 d = daq.getDevices;
  dev = d(4);
  %%
+ p=0;
  s = daq.createSession('directsound');
  s.IsContinuous=true;
 noutchan = 2;
 addAudioOutputChannel(s, dev.ID, 1:noutchan);
 s.Rate = fs;
 while(p<10)
-    queueOutputData(s, x, 1, noutchan);
- 
+    f=zeros(1600,1);
+    queueOutputData(s, repmat(f, 1, noutchan));
+
 p=p+1;
 end
-startBackground(s);
+p=0;
+ startBackground(s);
+
+while(p<2)
+    hwhite = dsp.ColoredNoise('Color','pink','SamplesPerFrame',fs);
+    rng default;
+    x = step(hwhite);
+    x=x./max(abs(x));
+    x=x./1.1;
+    %x=x.*(p+1)/2;
+    queueOutputData(s, repmat(x, 1, noutchan));
+
+p=p+1;
+end
