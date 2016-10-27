3ca08d01b1c923ad14f0f045372fb5c200d5a03b
Madhur <mxm916@miami.edu>
2016-10-26 21:10:56 -0400

    Made into function that can be used

---
+++ PinkNoiseGenerator.m
@@ -0,0 +1,13 @@
+function out = PinkNoiseGenerator(testSignal,samplingRate,maskerLevel_DBFS,maskeeLevel_DBFS)
+fs=samplingRate;
+   maskerLevel=dbtomag(maskerLevel_DBFS);
+   maskeeLevel=dbtomag(maskeeLevel_DBFS);
+ hpink = dsp.ColoredNoise('Color','pink','SamplesPerFrame',size(testSignal,1)+fs);
+    rng default;
+     pink_noise = step(hpink);
+    pink_noise=pink_noise./max(abs(pink_noise));
+    testSignal=[zeros(fs/2,1) testSignal zeros(fs/2,1)];
+    outAudio=maskerLevel.*pink_noise+maskeeLevel.*testSignal;
+    soundsc(outAudio,fs);
+out=1.0;
+end
