5e9c1204f9a1ba9481435d6eb95694f173478f6c
evanshenkman <evan.shenkman@gmail.com>
2016-10-31 15:58:58 -0400

    Organized files into folders

--- AlarmsTest.m
+++ AlarmsTest.m
@@ -1,5 +1,10 @@
-addpath('alarms/');
-addpath('subjects/');
+addpath(['src' filesep]);
+addpath(['src' filesep 'functions']);
+addpath(['src' filesep 'objects']);
+
+addpath(['GUIs' filesep]);
+addpath(['alarms' filesep]);
+addpath(['subjects' filesep]);
 
 if isempty(TestConfig.CalibrationLevel) || isnan(TestConfig.CalibrationLevel)
     TestConfig.CalibrationLevel(100);
